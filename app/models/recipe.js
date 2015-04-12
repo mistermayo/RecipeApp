27e5f6e5a1d8f3bad88d1d16693fa8131a2eb64d
Tucker <tuckerjamesmayo@gmail.com>
2015-04-11 22:40:50 -0700

    add recipes to category function

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -2,6 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  recipes: DS.hasMany('recipe')
-
+  category: DS.belongsTo('category')
 });
--- tests/integration/categories-page-test.js
+++ tests/integration/categories-page-test.js
@@ -11,21 +11,30 @@ module('Integration - Category Page', {
     var categories = [
       {
         id: 1,
-        title: 'Budget Cooking'
+        title: 'Budget Cooking', recipe_ids: [1,2]
       },
       {
         id: 2,
-        title: 'Vegetarian Recipes'
+        title: 'Vegetarian Recipes', recipe_ids: [3]
       },
       {
         id: 3,
-        title: 'Paleo'
+        title: 'Paleo', recipe_ids: [4,5,6]
       }
+    ];
+
+    var recipes = [
+      { id: 1, title: "Fast eating?", category_id: 1 },
+      { id: 2, title: "Meat on a budget", category_id: 1 },
+      { id: 3, title: "veggies and tofu.", category_id: 2 },
+      { id: 4, title: "no more dairy", category_id: 3 },
+      { id: 5, title: "chicken broth is the new milk.", category_id: 3 },
+      { id: 6, title: "yummers in my caveman tummers", category_id: 3 }
     ];
 
     server = new Pretender(function() {
       this.get('/api/categories', function(request) {
-        return [200, {"Content-Type": "application/json"}, JSON.stringify({categories: categories})];
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({categories: categories, recipes: recipes})];
       });
 
       this.get('/api/categories/:id', function(request) {
@@ -35,7 +44,7 @@ module('Integration - Category Page', {
           }
         });
 
-        return [200, {"Content-Type": "application/json"}, JSON.stringify({category: category})];
+        return [200, {"Content-Type": "application/json"}, JSON.stringify({category: category, recipes: recipes})];
       });
     });
 
@@ -54,13 +63,14 @@ test('Should allow navigation to the categories page from the landing page', fun
   });
 });
 
-test('Should list all categories', function(assert) {
+test('Should list all categories and number of recipes', function(assert) {
   visit('/categories').then(function() {
-    assert.equal(find('a:contains("Budget Cooking")').length, 1);
-    assert.equal(find('a:contains("Vegetarian Recipes")').length, 1);
-    assert.equal(find('a:contains("Paleo")').length, 1);
+    assert.equal(find('a:contains("Budget Cooking (2)")').length, 1);
+    assert.equal(find('a:contains("Vegetarian Recipes (1)")').length, 1);
+    assert.equal(find('a:contains("Paleo (3)")').length, 1);
   });
 });
+
 
 test('Should be able to navigate to a category page', function(assert) {
   visit('/categories').then(function() {
@@ -70,8 +80,11 @@ test('Should be able to navigate to a category page', function(assert) {
   });
 });
 
+
+
 test('Should be able visit a category page', function(assert) {
   visit('/categories/1').then(function() {
     assert.equal(find('h4').text(), 'Budget Cooking');
   });
+
 });
